f80191ff1170cf2c73e392faf057bb7fb729ea5e
Malak Mousa <116799883+Malakmousa99@users.noreply.github.com>
2024-08-08 12:40:27 +0300

    Time: 3 ms (29.51%) | Memory: 7.7 MB (6.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1);
+        vector<int> total(n+1);
 
-      dp[0]=1;
-      dp[1]=1;
+      total[0]=1;
+      total[1]=1;
 
        for(int i=2; i<=n ; i++){
-       dp[i] = dp[i-1] + dp[i-2];
+       total[i] =  total[i-1] +  total[i-2];
     
        }
-           return dp[n];
+           return  total[n];
     }
     
 };
